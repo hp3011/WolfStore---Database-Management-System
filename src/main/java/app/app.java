b26e434c5cb413010a29f4d2e4d9f5e2db7f3f19
jbatty <jbatty@ncsu.edu>
2021-04-01 22:00:12 -0400

    create tables function completed

--- src/main/java/app/app.java
+++ src/main/java/app/app.java
@@ -1,8 +1,10 @@
 package main.java.app;
 import java.sql.*;  
 import java.util.Scanner;
-import org.apache.ibatis.jdbc.ScriptRunner;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class app {
     static Connection conn;
@@ -16,7 +18,7 @@ public class app {
         conn = getConnection();
 
         // Create tables- loop through CREATE TABLE statements and execute each
-
+        createTables();
 
 
     }
@@ -30,8 +32,11 @@ public class app {
         System.out.print("Enter database password: ");
         password = in.nextLine();
         in.close();
-
-        Class.forName("org.mariadb.jdbc.Driver");
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+         } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
 
         try {
             return DriverManager.getConnection("jdbc:mariadb://classdb2.csc.ncsu.edu:3306/" + userid, userid, password);
@@ -40,4 +45,31 @@ public class app {
         }
     }
 
+    public static void createTables() {
+        Statement stmt = null;
+
+        try {
+            // Read in create table statements into an array
+            Path filePath = Paths.get("src", "main", "sql", "create_tables.txt");
+            String content = Files.readString(filePath);
+            String[] arrContent = content.split("\\r?\\n");
+
+            // Execute each SQL command
+            try {
+                stmt = conn.createStatement();
+            } catch (Exception e) { System.out.println(e); }
+
+            for (String a : arrContent){
+                try {
+                    System.out.println(a);
+                    stmt.executeUpdate(a);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
